4472ce09134803e1bc7a3e82aedf18ea2adbf813
A.S.M. Mahmudul Hasan <asmmahmud@gmail.com>
2018-01-04 22:57:01 +0600

    updating config

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const API_DOMAIN = 'https://infinite-ocean-25940.herokuapp.com';
-export const SPA_DOMAIN = 'http://localhost:3000';
+export const SPA_DOMAIN = 'https://clever-mcclintock-1cb012.netlify.com';
